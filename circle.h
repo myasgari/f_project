6e56acf65da1ded422f8b4dec7ba400c6b379886
myasgari <mohamadyasinasgari@gmail.com>
2019-01-08 10:54:36 +0330

    virtual funct added

---
+++ circle.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "shape.h"
+#include <string>
+class circle :public shape
+{
+public:
+	circle(const std::string &);
+	virtual void sayName() override;
+	virtual void setXandY() override;
+	virtual void setColor() override;
+	virtual void setSize() override;
+	virtual void setStorkeWidth() override;
+	~circle();
+private:
+	std::string name;
+};
