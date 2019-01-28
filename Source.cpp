4da2d78a8a1b02fb12e74789ddf45c4e25e4caf1
myasgari <mohamadyasinasgari@gmail.com>
2019-01-28 05:39:19 +0330

    the funct creat for creatig object added

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 enum choise { CREATE = 1, ERASE, ERASEALL, LIST, EXPORT };
+void create(vector<shape *> &);
 int enterChois();
 int main()
 {
--- prosess.cpp
+++ prosess.cpp
@@ -7,11 +7,26 @@
 #include <fstream>
 #include <string>
 using namespace std;
-
 int enterChois()
 {
 	cout << "1-create\t2-erase\t3-erase all\t4-list\t5-export" << endl;
 	char chose;
 	cin >> chose;
 	return chose;
+}
+void create(vector<shape *>&shape)
+{
+	cout << "which object do want to draw ?" << endl;
+	cout << "1-circle\t2-rect\tellipse" << endl;
+	int in;
+	cout << "enter the object name :";
+	string name;
+	cin >> name;
+	if (in == 1)
+		shape.push_back(new circle(name));
+	if (in == 2)
+		shape.push_back(new rect(name));
+	if (in == 3)
+		shape.push_back(new ellipse(name));
+	shape[shape::Count]->create();
 }
