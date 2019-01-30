b83d160748bd91cb140aa152071cdc4b8f907f61
myasgari <mohamadyasinasgari@gmail.com>
2019-01-30 10:04:31 +0330

    it better they become un virtual

--- shape.h
+++ shape.h
@@ -5,10 +5,11 @@ class shape
 public:
 	shape();
 	virtual void create() = 0 ;
-	virtual void setXandY() = 0;
-	virtual void setColor() = 0;
-	virtual void setSize() = 0;
-	virtual void setStorkeWidth() = 0;
+	virtual void EXPORT() =0 ;
+//	virtual void setXandY() = 0;
+//	virtual void setColor() = 0;
+//	virtual void setSize() = 0;
+//	virtual void setStorkeWidth() = 0;
 	virtual void sayName() = 0;
 	static unsigned int Count;
 	~shape();
